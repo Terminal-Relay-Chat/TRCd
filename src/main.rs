205f2ad404bb4ef2d05dc98673088b2e9382a36b
Master Penguin <137585700+robot-penguin34@users.noreply.github.com>
2025-12-28 19:55:26 -0700

    make socket server and api run simultaniously

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use tokio;
+use log::{warn, info};
 mod server;
 mod socket_server;
 
@@ -7,7 +8,11 @@ async fn main() {
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .init();
+    
     let server = server::Server::new(3000);
-    //TODO: implement the socket_server
-    server.run().await;
+    let socks = socket_server::SocketServer::new(3001);
+    tokio::select! {
+        _ = server.run() => { warn!("server crashed!") },
+        _ = socks.run() => { warn!("socket server crashed!") },
+    }
 }
