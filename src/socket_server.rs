f70e50a66e0baad71babd133ef35318c15b1cdd9
Master Penguin <137585700+robot-penguin34@users.noreply.github.com>
2025-12-28 21:11:00 -0700

    basic (and ugly) socket setup

--- src/socket_server.rs
+++ src/socket_server.rs
@@ -1,18 +1,21 @@
 //! The Socket server for TRCd is what publishes updates to clients
 
-use std::net::SocketAddr;
+use std::{error::Error, net::SocketAddr};
 
 use serde::{Serialize, Deserialize};
-use axum::{extract::{ConnectInfo, WebSocketUpgrade, ws::WebSocket}, response::IntoResponse, routing::any};
+use axum::{extract::{ConnectInfo, WebSocketUpgrade, ws::{CloseFrame, WebSocket, close_code::UNSUPPORTED}}, response::IntoResponse, routing::any};
 use axum::extract::ws::Message;
-use log::{info, warn};
+use log::{info, warn, trace};
 use futures_util::{SinkExt, stream::StreamExt};
+use std::sync::Arc;
+
+const MAX_STUPID_MESSAGE: u8 = 10; // to prevent useless data abuse
 
 #[derive(Debug, Serialize, PartialEq)]
 enum UpdateType {
     MESSAGE,
     SYSTEM, // SYSTEM is for commands or responses to requests from a client
-    ERROR
+    ERROR,
 }
 
 #[derive(Serialize, Debug)]
@@ -50,11 +53,58 @@ impl SocketServer {
     }
 
     async fn handle_socket(mut sock: WebSocket, ip: SocketAddr) {
-        loop {
-            sock.send(Message::text("Hello World!")).await;
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        }
-    }
+        use tokio::sync::Mutex;
 
+        async fn handle_sock_recv(ws: Arc<Mutex<WebSocket>>, ip: &SocketAddr) -> Result<(), Box<dyn Error>> {
+            let mut stupid_message_counter: u8 = 0; // prevent useless message abuse
+            loop {
+                let message = match ws.lock().await.recv().await {
+                    Some(m) => m,
+                    None => {return Ok(())}
+                };
+                let message = match message {
+                    Ok(m) => m,
+                    Err(e) => {return Err(Box::new(e))},
+                };
+                match message {
+                    Message::Close(_) => {
+                        ws.lock().await.send(Message::Close(None)).await?;
+                        trace!("client sent close");
+                        return Ok(())
+                    },
+                    Message::Ping(payload) => {
+                        ws.lock().await.send(Message::Pong(payload)).await?;
+                    },
+                    Message::Text(t) => {
+                        info!("Message from client: {}", t);
+                    },
+                    _ => {
+                        stupid_message_counter += 1;
+                        if stupid_message_counter > MAX_STUPID_MESSAGE {
+                            warn!("Weird data exceeded threshold from ip: {}", *ip);
+                            ws.lock().await.send(Message::Close(Some(CloseFrame {
+                                code: UNSUPPORTED,
+                                reason: "This server supports pings and text (utf-8 formatted).".into()
+                            }))).await?;
+
+                            return Ok(());
+                        }
+                    }
+
+                }
+            }
+        }
+
+        async fn handl_sock_send(ws: Arc<Mutex<WebSocket>>) -> Result<(), Box<dyn std::error::Error>> {
+            Ok(())
+        }
+        
+        let ws = Arc::new(Mutex::new(sock));
+        tokio::select! {
+            _ = handle_sock_recv(ws.clone(), &ip) => {},
+            // _ = handl_sock_send(ws.clone()) => {}
+        }
+        info!("client disconnected")
+    }
 
 }
