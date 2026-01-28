fb181880c6a59cc64ed79232997dd67e7aa4534c
Master Penguin <137585700+robot-penguin34@users.noreply.github.com>
2026-01-27 17:02:07 -0700

    more compatibility

--- src/backend/socket_server.rs
+++ src/backend/socket_server.rs
@@ -212,7 +212,7 @@ impl SocketServer {
 
                         // send a response to the user
                         let success_response = serde_json::json!({
-                            "message_type": UpdateType::MESSAGE,
+                            "message_type": UpdateType::SYSTEM,
                             "error": false,
                             "content": "successfully changed channel",
                             "value": Some(UserActiveChannel::String(t.to_string()))
